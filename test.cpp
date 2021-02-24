c1e612d6320ef14aa2fa42e7be7a99084f5e7672
Marisa Heit <rheit@users.noreply.github.com>
2021-02-24 09:35:30 -0600

    Added Wolfgang Lorenz's C64 Test Suite
    This is the slightly modified version of the suite from VICE because
    I needed a version that would read the "magic constant" for the
    unstable opcodes instead of assuming one. The test suite passes through
    the cputiming tests. Getting further requires a more accurate CIA
    emulation, which may or may not be worthwhile. The irq and nmi tests
    sound like they might be useful, but I think the rest are unnecessary
    for testing just a 6502.

--- test.cpp
+++ test.cpp
@@ -2,6 +2,11 @@
 #include <fstream>
 #include <iostream>
 #include <cerrno>
+#include <cmath>
+#include <cstring>
+#include <string_view>
+#include <filesystem>
+#include <algorithm>
 
 #include "yam6502.h"
 
@@ -42,6 +47,20 @@ enum class IO : uint16_t {
 	InterruptPort = 0xff8c,
 };
 
+void dump_mem(FILE *out, const uint8_t *memory, uint16_t start, uint16_t end)
+{
+	for (auto i = start; i < end; ++i) {
+		if (!(i & 15)) {
+			fprintf(out, "\n%04X:", i);
+		}
+		else if (!(i & 3)) {
+			fputc(' ', out);
+		}
+		fprintf(out, " %02X", memory[i]);
+	}
+	fputc('\n', out);
+}
+
 struct TestBus {
 	uint8_t readAddr(uint16_t addr)
 	{
@@ -62,18 +81,9 @@ struct TestBus {
 	}
 	void dump_mem(uint16_t start, uint16_t end) const
 	{
-		for (auto i = start; i < end; ++i) {
-			if (!(i & 15)) {
-				printf("\n%04X:", i);
-			}
-			else if (!(i & 3)) {
-				printf(" ");
-			}
-			printf(" %02X", memory[i]);
-		}
-		printf("\n");
+		::dump_mem(stdout, memory, start, end);
 	}
-	uint8_t memory[65536];
+	uint8_t memory[65536]{};
 	uint16_t last_addr = ~0;
 };
 
@@ -87,6 +97,531 @@ struct TestBusWithInterrupts : public TestBus {
 		return !(memory[static_cast<int>(IO::InterruptPort)] & 2);
 	}
 };
+
+class MiniCIA {
+public:
+	// Control register bits
+	  static constexpr inline uint8_t START		= 0x01;
+	//static constexpr inline uint8_t PBON		= 0x02;
+	//static constexpr inline uint8_t OUTMODE	= 0x04;
+	  static constexpr inline uint8_t ONESHOT	= 0x08;
+	  static constexpr inline uint8_t LOAD		= 0x10;
+	//static constexpr inline uint8_t INMODE	= 0x20;		// only phi2 (0) supported
+	//static constexpr inline uint8_t SPMODE	= 0x40;
+	//static constexpr inline uint8_t TODIN		= 0x80;
+
+	// Different bits for Control Register B
+	  static constexpr inline uint8_t INMODEB	= 0x40;
+	//static constexpr inline uint8_t ALARM		= 0x80;
+
+	[[nodiscard]] uint8_t readReg(int reg)
+	{
+		switch (reg) {
+		case 4:		return TimerA & 0xFF;
+		case 5:		return TimerA >> 8;
+		case 6:		return TimerB & 0xFF;
+		case 7:		return TimerB >> 8;
+		case 13:  { uint8_t ret = IrqData & IrqMask; IrqData = 0; return ret; }
+		case 14:	return CRA;
+		case 15:	return CRB;
+		}
+		return 0;
+	}
+
+	void writeReg(int reg, uint8_t data)
+	{
+		switch (reg) {
+		case 4:		setTimerLo(data, TimerALatch); break;
+		case 5:		setTimerHi(data, TimerALatch, CRA); break;
+		case 6:		setTimerLo(data, TimerBLatch); break;
+		case 7:		setTimerHi(data, TimerBLatch, CRB); break;
+		case 13:	writeIrqMask(data); break;
+		case 14:	CRA = data; break;
+		case 15:	CRB = data; break;
+		}
+	}
+
+	void tick()
+	{
+		runTimerA();
+		runTimerB();
+	}
+
+	[[nodiscard]] uint16_t getTimerA() const
+	{
+		return TimerA;
+	}
+
+	[[nodiscard]] uint16_t getTimerB() const
+	{
+		return TimerB;
+	}
+
+	[[nodiscard]] bool getIRQB() const
+	{
+		return !(IrqData & IrqMask);
+	}
+
+private:
+	uint16_t TimerALatch = 0, TimerA = 0;
+	uint16_t TimerBLatch = 0, TimerB = 0;
+	uint8_t CRA = 0, CRB = 0;
+	uint8_t IrqData = 0, IrqMask = 0x81;
+	bool TimerAOut = false;
+
+	void setTimerLo(uint8_t data, uint16_t &latch)
+	{
+		latch = (latch & 0xFF00) | data;
+	}
+
+	void setTimerHi(uint8_t data, uint16_t &latch, uint8_t &cr)
+	{
+		latch = (latch & 0x00FF) | (data << 8);
+		if (!(cr & START)) {
+			cr |= LOAD;
+		}
+	}
+
+	void writeIrqMask(uint8_t mask)
+	{
+		if (mask & 0x80) {
+			IrqMask |= mask & 0x03;
+		}
+		else {
+			IrqMask &= ~(mask & 0x03);
+		}
+		if (IrqMask & 0x7F) {
+			IrqMask |= 0x80;
+		}
+		else {
+			IrqMask = 0;
+		}
+	}
+
+	void runTimerA() {
+		TimerAOut = false;
+		if (CRA & LOAD) {
+			TimerA = TimerALatch;
+			CRA &= ~LOAD;
+			return;
+		}
+		if (CRA & START) {
+			if (TimerA-- == 0) {
+				TimerA = TimerALatch;
+				TimerAOut = true;
+				IrqData |= 0x81;
+				if (CRA & ONESHOT) {
+					CRA &= ~START;
+				}
+			}
+		}
+	}
+
+	void runTimerB() {
+		if (CRB & LOAD) {
+			TimerB = TimerBLatch;
+			CRB &= ~LOAD;
+			return;
+		}
+		if (CRB & START) {
+			if ((!(CRB & INMODEB) || TimerAOut) && TimerB-- == 0) {
+				TimerB = TimerBLatch;
+				IrqData |= 0x82;
+				if (CRB & ONESHOT) {
+					CRB &= ~START;
+				}
+			}
+		}
+	}
+};
+
+struct MiniC64Bus {
+	constexpr static inline int TRAP = 2;
+
+	constexpr static inline uint16_t FACHO = 0x62;
+
+	constexpr static inline uint16_t FNLEN = 0xB7;
+	constexpr static inline uint16_t SA = 0xB9;			// Secondary address for IO
+	constexpr static inline uint16_t FNADR = 0xBB;
+	constexpr static inline uint16_t PNTR = 0xD3;		// Cursor column on current line
+
+	constexpr static inline uint16_t CBINV = 0x0316;	// BRK interrupt vector
+
+	constexpr static inline uint16_t DONEADDR = 0x8000;	// Tests jump here when done
+	//constexpr static inline uint16_t WARMVEC = 0xA002;	// BASIC warm start vector
+	//constexpr static inline uint16_t READY = 0xA474;	// Enter BASIC immediate mode
+
+	constexpr static inline uint16_t STROUT = 0xAB1E;	// Print 0-terminated string at (Y,A)
+	constexpr static inline uint16_t FLOATC = 0xBC49;	// Float unsigned value in FAC+1,2
+	constexpr static inline uint16_t FMULT = 0xBA28;	// Multiply FP accum with memory
+	constexpr static inline uint16_t MOVAF = 0xBC0C;	// Move FP accum to FP arg
+	constexpr static inline uint16_t FADDT = 0xB86A;	// Add FP arg to FP accum
+	constexpr static inline uint16_t LINPRNT = 0xBDCD;	// Print XA as unsigned integer
+	constexpr static inline uint16_t FOUT = 0xBDDD;		// FP accum to string at bottom of stack
+
+	constexpr static inline uint16_t SCROLY = 0xD011;
+
+	constexpr static inline uint16_t CIA2CRA = 0xDD0E;	// CIA 2 control register A
+	constexpr static inline uint16_t CIA2CRB = 0xDD0F;	// CIA 2 control register B
+
+	constexpr static inline uint16_t LOAD_INTERNAL = 0xE16F; // branchwrap test jumps here
+
+	constexpr static inline uint16_t PULS = 0xFF48;		// IRQ handler
+	constexpr static inline uint16_t SETNAM = 0xFDF9;	// Set filename parameters
+
+	constexpr static inline uint16_t VEC_IOINIT = 0xFF84;
+	constexpr static inline uint16_t VEC_RESTOR = 0xFF8A;	// Restore default system and interrupt vectors
+	constexpr static inline uint16_t VEC_SETLFS = 0xFFBA;	// Set up a logical file
+	constexpr static inline uint16_t VEC_SETNAM = 0xFFBD;
+	constexpr static inline uint16_t VEC_GETIN = 0xFFE4;
+	constexpr static inline uint16_t VEC_CHROUT = 0xFFD2;
+	constexpr static inline uint16_t VEC_LOAD = 0xFFD5;	// Load RAM from a device
+
+	using cputype = m65xx::M6502<MiniC64Bus *, TRAP>;
+
+	uint8_t memory[65536]{};
+	MiniCIA CIA1;
+	MiniCIA CIA2;
+	double fp_accum = 0;
+	double fp_arg = 0;
+
+	enum class State {
+		Running,
+		Passed,
+		Failed
+	} state = State::Running;
+
+	void tick();
+	void init(cputype &cpu);
+	void startTest(cputype &cpu, uint16_t loadaddr);
+	int loadTest(std::string_view testname, bool reloc, uint16_t loadaddr);
+	static std::string pet2ascii(uint8_t pet);
+	bool breakHandler(cputype &cpu, uint16_t addr);
+	bool trap(cputype &cpu, uint16_t addr);
+	[[nodiscard]] uint16_t readWord(uint16_t addr) const;
+	void writeWord(uint16_t addr, uint16_t data);
+	uint8_t readAddr(uint16_t addr);
+	void writeAddr(uint16_t addr, uint8_t data);
+
+	[[nodiscard]] bool getIRQB() const
+	{
+		return CIA1.getIRQB();
+	}
+	[[nodiscard]] bool getNMIB() const
+	{
+		return CIA2.getIRQB();
+	}
+};
+
+void MiniC64Bus::tick()
+{
+	CIA1.tick();
+	CIA2.tick();
+}
+
+void MiniC64Bus::init(cputype &cpu)
+{
+	memory[DONEADDR] = TRAP;
+	memory[LOAD_INTERNAL] = TRAP;
+
+	memory[VEC_SETNAM] = 0x4C;	// JMP
+	writeWord(VEC_SETNAM + 1, SETNAM);
+
+	memory[VEC_IOINIT] = 0x60;		// RTS because not relevant here
+
+	for (auto traploc :
+		{ VEC_RESTOR, VEC_LOAD, VEC_CHROUT,
+			FLOATC, FMULT, MOVAF, FADDT, FOUT, STROUT, LINPRNT }) {
+		memory[traploc] = TRAP;
+		memory[traploc + 1] = 0x60;	// RTS
+	}
+
+	// The secondary address, which indicates whether to relocate
+	// the load, is the only thing we care about for SETLFS.
+	memory[VEC_SETLFS] = 0x84;		// STY zp
+	memory[VEC_SETLFS + 1] = SA;
+	memory[VEC_SETLFS + 2] = 0x60;	// RTS
+
+	// Make GETIN always return #3
+	memory[VEC_GETIN] = 0xA9;	// LDA #
+	memory[VEC_GETIN + 1] = 3;
+	memory[VEC_GETIN + 2] = 0x60;	// RTS
+
+	// Copy SETNAM routine
+	static const uint8_t setnam[] = {
+		0x85, FNLEN,		// STA FNLEN
+		0x86, FNADR,		// STX FNADR
+		0x84, FNADR + 1,	// STY FNADR+1
+		0x60				// RTS
+	};
+	std::copy(setnam, setnam + sizeof(setnam), &memory[SETNAM]);
+
+	// Setup vectors
+	//writeWord(WARMVEC, FAILADDR);
+	writeWord(cpu.opToVector(m65xx::op::IRQ), PULS);
+	writeWord(CBINV, DONEADDR);
+
+	// Copy IRQ handler
+	static const uint8_t puls[] = {
+		0x48,				// PHA
+		0x8A,				// TXA
+		0x48,				// PHA
+		0x98,				// TYA
+		0x48,				// PHA
+		0xBA,				// TSX
+		0xBD, 0x04, 0x01,	// LDA $0104,X
+		0x29, 0x10,			// AND #$10
+		0xF0, 0x03,			// BEQ $FF58
+		0x6C, 0x16, 0x03,	// JMP ($0316)
+		0x6C, 0x14, 0x03,	// JMP ($0314)
+	};
+	std::copy(puls, puls + sizeof(puls), &memory[PULS]);
+}
+
+void MiniC64Bus::startTest(cputype &cpu, uint16_t loadaddr)
+{
+	init(cpu);
+
+	// Set up top of stack so RTS goes to DONEADDR
+	writeWord(0x1FE, DONEADDR - 1);
+	cpu.setSP(0x1FD);
+	cpu.setP(m65xx::FLAG_I);
+
+	// Find the real program immediately after the BASIC stub.
+	auto addr = readWord(loadaddr);
+	while (auto next = readWord(addr)) {
+		if (next != 0) {
+			addr = next;
+		}
+	}
+	// Start is the first non-0 byte
+	addr += 2;
+	while (memory[addr] == 0) {
+		++addr;
+	}
+	cpu.setPC(addr);
+}
+
+// Returns address file was loaded to, or -1 on failure.
+int MiniC64Bus::loadTest(std::string_view testname, bool reloc, uint16_t loadaddr)
+{
+	try {
+		std::filesystem::path path = "tests/bin/lorenz";
+		// Convert file name from PETSCII to ASCII. In practice, this
+		// amounts to converting from uppercase to uppercase.
+		std::string name_ascii;
+		name_ascii.reserve(testname.size());
+		for (auto c : testname) {
+			name_ascii += pet2ascii(c);
+		}
+		path /= name_ascii;
+		path += ".prg";
+		auto size = std::filesystem::file_size(path) - 2;
+		std::ifstream input(path, std::ios::binary);
+		uint8_t loadlo, loadhi;
+		input >> loadlo >> loadhi;
+		if (!reloc) {
+			loadaddr = (loadhi << 8) | loadlo;
+		}
+		size = std::min<decltype(size)>(65536 - loadaddr, size);
+		if (input.read(reinterpret_cast<char *>(&memory[loadaddr]), size)) {
+			return loadaddr;
+		}
+	}
+	catch (const std::runtime_error &error) {
+		std::cerr << error.what() << '\n';
+	}
+	return -1;
+}
+
+std::string MiniC64Bus::pet2ascii(uint8_t pet)
+{
+	if (pet == 147/* Clear screen */ || pet == 14/* Switch to lower case */) {
+		return "";
+	}
+	if (pet == 145) {	// Cursor up
+		return "\33[A";
+	}
+	if (pet >= 'A' && pet <= 'Z') {
+		return std::string(1, pet + ('a' - 'A'));
+	}
+	if (pet >= 'a' && pet <= 'z') {
+		return std::string(1, pet + ('A' - 'a'));
+	}
+	if (pet >= 0xC1 && pet <= 0xDA) {
+		return std::string(1, pet - (0xC1 - 'A'));
+	}
+	if (pet == '\r') {
+		return "\n";
+	}
+	return std::string(1, pet);
+}
+
+bool MiniC64Bus::breakHandler(cputype &cpu, uint16_t addr)
+{
+	if (addr >= DONEADDR) {
+		fprintf(stderr, "Unhandled routine at $%04X\n", addr);
+		fprintf(stderr, "Zero page dump:\n");
+		dump_mem(stderr, memory, 0, 256);
+		state = State::Failed;
+		return true;
+	}
+	return false;
+}
+
+bool MiniC64Bus::trap(cputype &cpu, uint16_t addr)
+{
+	switch (addr) {
+	//case READY: 
+//			state = State::Passed;
+//		return true;
+
+	case DONEADDR:
+		state = State::Failed;
+		return true;
+
+	case VEC_RESTOR:
+		init(cpu);
+		return true;
+
+	case VEC_LOAD: {
+		std::string_view test{ reinterpret_cast<char *>(&memory[readWord(FNADR)]), memory[FNLEN] };
+		if (loadTest(test, !!memory[SA], (cpu.getX() << 8) | cpu.getY()) < 0) {
+			std::cerr << "Failed loading " << test << '\n';
+			state = State::Failed;
+		}
+		return true;
+	}
+
+	case LOAD_INTERNAL:
+		// Branchwrap test jumps to here to start mmufetch test.
+		// That and the mmu test are C64-specific, so jump to the
+		// cputiming test.
+		if (auto addr = loadTest("cputiming", false, 0); addr >= 0) {
+			startTest(cpu, addr);
+		}
+		else {
+			std::cerr << "Failed loading cputiming\n";
+			state = State::Failed;
+		}
+		return true;
+
+	case VEC_CHROUT:
+		std::cout << pet2ascii(cpu.getA());
+		return true;
+
+	// Various BASIC routines cputiming uses to print a 32-bit number
+	case FLOATC:
+		// FACHO[0] and FACHO[1] contain an unsigned 16-bit integer
+		// in big-endian order. X contains the MBF (Microsoft Binary
+		// Format) exponent. C is set for a positive number, clear for
+		// a negative number. We store the result in a private
+		// accumulator in the host's native floating point format
+		// rather than the zero page MBF accumulator.
+		fp_accum = ((memory[FACHO] << 8) | memory[FACHO + 1]) * std::exp2(cpu.getX() - (128 + 16));
+		if (!(cpu.getP() & m65xx::FLAG_C)) {
+			fp_accum = -fp_accum;
+		}
+		return true;
+
+	case FMULT: {
+		static_assert(sizeof(double) == 8 && std::numeric_limits<double>::is_iec559);
+		// A is the low byte of the memory arg's address
+		// Y is the high byte of the memory arg's address
+		//
+		// On the C64, this is a 5-byte MBF floating point number,
+		// which is very similar to IEEE-754. The first byte is the
+		// exponent, 128 biased. The next bit is the sign. Then the next
+		// 32 bits are are the mantissa, in big-endian format. There is an
+		// implicit 1 just after the radix point (compared to just before
+		// it for IEEE-754).
+		uint16_t addr = (cpu.getY() << 8) | cpu.getA();
+		const uint8_t *mbf = &memory[addr];
+		if (mbf[0] == 0) {
+			fp_arg = 0;
+		} else {
+			uint64_t fpbits =
+				(uint64_t(mbf[1] & 0x80) << 56)
+				| (uint64_t(mbf[0] + (1023 - 129)) << 52)
+				| (uint64_t(mbf[1] & 0x7F) << 45)
+				| (uint64_t(mbf[2]) << 37)
+				| (uint64_t(mbf[3]) << 29)
+				| (uint64_t(mbf[4]) << 21);
+			std::memcpy(&fp_arg, &fpbits, 8);
+			}
+		fp_accum *= fp_arg;
+		return true;
+	}
+
+	case MOVAF:
+		fp_arg = fp_accum;
+		return true;
+
+	case FADDT:
+		fp_accum += fp_arg;
+		return true;
+
+	case FOUT:
+		snprintf(reinterpret_cast<char *>(&memory[0x100]), 32, "% g", fp_accum);
+		cpu.setY(1);
+		cpu.setA(0);
+		return true;
+
+	case STROUT:
+		printf("%s", &memory[(cpu.getY() << 8) | cpu.getA()]);
+		return true;
+
+	case LINPRNT:
+		printf("%u", (cpu.getX() << 8) | cpu.getA());
+		return true;
+	}
+	return false;
+}
+
+[[nodiscard]] uint16_t MiniC64Bus::readWord(uint16_t addr) const
+{
+	return memory[addr] + (memory[addr + 1] << 8);
+}
+
+void MiniC64Bus::writeWord(uint16_t addr, uint16_t data)
+{
+	memory[addr] = data & 0xFF;
+	memory[addr + 1] = data >> 8;
+}
+
+uint8_t MiniC64Bus::readAddr(uint16_t addr)
+{
+	if (addr == SCROLY) {
+		// Always say we're inside the border
+		return 0x80;
+	}
+	if ((addr & 0xFF00) == 0xDC00) {
+		return CIA1.readReg(addr & 15);
+	}
+	if ((addr & 0xFF00) == 0xDD00) {
+		return CIA2.readReg(addr & 15);
+	}
+	return memory[addr];
+}
+
+void MiniC64Bus::writeAddr(uint16_t addr, uint8_t data)
+{
+	if (addr == PNTR) {
+		std::cout << '\r';
+		if (data != 0) {
+			printf("\33[%uC", data);
+		}
+	}
+	else if ((addr & 0xFF00) == 0xDC00) {
+		CIA1.writeReg(addr & 15, data);
+	}
+	else if ((addr & 0xFF00) == 0xDD00) {
+		CIA2.writeReg(addr & 15, data);
+	}
+	else {
+		memory[addr] = data;
+	}
+}
 
 void print_p(unsigned p)
 {
@@ -127,7 +662,7 @@ void run_functional_test()
 			bus.dump_mem(data_segment, data_bss_end);
 			[[maybe_unused]] int i = 0;	// Error!
 		}
-		if (0 && cpu.getSync()) {
+		if (false && cpu.getSync()) {
 			printf("A=%02X X=%02X Y=%02X S=%02X ",
 				cpu.getA(), cpu.getX(), cpu.getY(), cpu.getSP());
 			print_p(cpu.getP());
@@ -209,10 +744,38 @@ void run_decimal_test()
 	}
 }
 
+void run_lorenz_tests()
+{
+	printf("Running Wolfgang Lorenz's test suite\n");
+	MiniC64Bus bus;
+	MiniC64Bus::cputype cpu(&bus);
+	if (auto loadaddr = bus.loadTest("START", false, 0)) {
+		bus.startTest(cpu, loadaddr);
+		while (bus.state == MiniC64Bus::State::Running) {
+			cpu.tick();
+			bus.tick();
+			if (false && cpu.getSync()) {
+				printf("A=%02X X=%02X Y=%02X S=%02X",
+					cpu.getA(), cpu.getX(), cpu.getY(), cpu.getSP());
+				print_p(cpu.getP());
+				printf(" T1A=%-5u %s\n", bus.CIA1.getTimerA(),
+					cpu.disasmOp(cpu.getPC() - 1, true).c_str());
+			}
+		}
+		if (bus.state == MiniC64Bus::State::Failed) {
+			std::cout << "  Failed\n";
+		}
+		else {
+			std::cout << "  Ok\n";
+		}
+	}
+}
+
 int main()
 {
 	run_functional_test();
 	run_decimal_test();
 	run_interrupt_test();
+	run_lorenz_tests();
 	return 0;
 }
